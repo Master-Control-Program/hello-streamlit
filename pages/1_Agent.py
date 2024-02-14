33c7ec21bf5f47a108c2167231b8c62ac1311ddb
Brad Meadows <bradley.meadows@cnoinc.com>
2024-02-14 13:46:03 -0500

    Agent Update

--- pages/1_Agent.py
+++ pages/1_Agent.py
@@ -1,56 +1,33 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import time
-
-import numpy as np
-
 import streamlit as st
-from streamlit.hello.utils import show_code
+import pandas as pd
 
+# Set page title
+st.set_page_config(page_title="Region")
 
-def plotting_demo():
-    progress_bar = st.sidebar.progress(0)
-    status_text = st.sidebar.empty()
-    last_rows = np.random.randn(1, 1)
-    chart = st.line_chart(last_rows)
+# Text input boxes
+description = st.text_input("Description", "")
+manager_name = st.text_input("Manager Name", "")
 
-    for i in range(1, 101):
-        new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-        status_text.text("%i%% Complete" % i)
-        chart.add_rows(new_rows)
-        progress_bar.progress(i)
-        last_rows = new_rows
-        time.sleep(0.05)
+# Search and Clear Buttons
+col1, col2 = st.columns([1, 1])
+if col1.button("Search"):
+    # Perform search action
+    pass  # Placeholder for search action
+if col2.button("Clear"):
+    description = ""
+    manager_name = ""
 
-    progress_bar.empty()
+# Sample table data
+data = {
+    "Region": ["Region A", "Region B", "Region C"],
+    "Description": ["Description A", "Description B", "Description C"],
+    "Manager Name": ["Manager A", "Manager B", "Manager C"]
+}
+df = pd.DataFrame(data)
 
-    # Streamlit widgets automatically run the script from top to bottom. Since
-    # this button is not connected to any other logic, it just causes a plain
-    # rerun.
-    st.button("Re-run")
+# Filter data based on input
+filtered_df = df[(df["Description"].str.contains(description, case=False)) & 
+                 (df["Manager Name"].str.contains(manager_name, case=False))]
 
-
-st.set_page_config(page_title="Plotting Demo", page_icon="📈")
-st.markdown("# Plotting Demo")
-st.sidebar.header("Plotting Demo")
-st.write(
-    """This demo illustrates a combination of plotting and animation with
-Streamlit. We're generating a bunch of random numbers in a loop for around
-5 seconds. Enjoy!"""
-)
-
-plotting_demo()
-
-show_code(plotting_demo)
+# Display table
+st.table(filtered_df)
