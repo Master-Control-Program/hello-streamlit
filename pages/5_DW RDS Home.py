6b4f9d7607b017219410d3a68268a351f8069816
Brad Meadows <bradley.meadows@cnoinc.com>
2024-02-21 13:43:20 -0500

    docx

--- pages/5_DW RDS Home.py
+++ pages/5_DW RDS Home.py
@@ -1,27 +1,26 @@
-
-
 import streamlit as st
 import os
 from docx import Document
+from docx.opc.exceptions import PackageNotFoundError
 
-# Set page title
-st.set_page_config(page_title="DW RDS Home",layout="wide")
-
-st.write("This page/section was linked to a word document that is listed below.")
-
-
-
-
-# Path to the .doc file
+# Path to the .docx file
 file_path = r'C:\Users\bradm\Documents\GitHub\hello-streamlit\.documents\RDS_TMApp_User_Manual.docx'
 
-# Load the document
-doc = Document(file_path)
+# Check if the file exists
+if not os.path.exists(file_path):
+    st.error("File not found.")
+else:
+    try:
+        # Load the document
+        doc = Document(file_path)
 
-# Extract text from the document
-text = "\n".join([paragraph.text for paragraph in doc.paragraphs])
+        # Extract text from the document
+        text = "\n".join([paragraph.text for paragraph in doc.paragraphs])
 
-# Display the text using Streamlit
-st.text(text)
+        # Display the text using Streamlit
+        st.text(text)
+    except PackageNotFoundError:
+        st.error("Invalid .docx file format.")
+
 
 
